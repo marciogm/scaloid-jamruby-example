58969ffd9cc5718edbe5f08301d18f98674e538b
marciogm <marciogiaxa@gmail.com>
2013-10-15 01:58:13 -0300

    i can get the context with this way, but i got error when the app is run

--- src/org/scaloid/hello/HelloScaloid.scala
+++ src/org/scaloid/hello/HelloScaloid.scala
@@ -4,29 +4,30 @@ import org.scaloid.common._
 import android.graphics.Color
 import java.io.File
 import java.io.FileFilter
+import android.content.Context
+import android.app._
+import android.content._
+import android.os._
+import android.view._
  
 import org.jamruby.core.Jamruby
 import org.jamruby.mruby.MRuby
 import org.jamruby.mruby.ParserState
 import org.jamruby.mruby.Value
  
-object TestScalaObject extends HelloScaloid with SContext with SActivity {
+object TestScalaObject extends HelloScaloid {
 	val TEST_SYMBOL = "*"
-	var ctx :SVerticalLayout = _;
+	override implicit val ctx = this
 	
-	override def onCreate() {
-		startActivity[HelloScaloid]
-	}
+	// def SetContext(c : Context) = {
+	// 	ctx = c
+	// }
+	// 
+	// def GetContext() : Context = {
+	// 	ctx
+	// }
 	
-	def SetContext(c : SVerticalLayout) = {
-		ctx = c
-	}
-	
-	def GetContext() : SVerticalLayout = {
-		ctx
-	}
-	
-	def testMethod(ctxx :SVerticalLayout) : String = {
+	def testMethod() = {
 		toast("marcio")
 	}
 }
@@ -34,16 +35,14 @@ object TestScalaObject extends HelloScaloid with SContext with SActivity {
 class HelloScaloid extends SActivity {
  
   onCreate {	
+	// TestScalaObject.SetContext(this)
+	
     contentView = new SVerticalLayout {
       style {
         // case b: SButton => b.textColor(Color.RED).onClick(loading())
         case t: STextView => t textSize 10.dip
         case v => v.backgroundColor(Color.YELLOW)
-      }
-	
-	  TestScalaObject.SetContext(this)
-	  
-	  
+      }  
 	  
 	  
 	  // javap -s -bootclasspath classes.jar -classpath bin/classes org.scaloid.common.SToast
@@ -59,27 +58,25 @@ class HelloScaloid extends SActivity {
   	  // 					r = to.call(o,im,"Hello, Ruby!",100)
   	  // 
 	  
-	  var code = """c = JAVA.find_class("org/scaloid/hello/HelloScaloid")
-	  				sm = c.get_static_method("testMethod","()Ljava/lang/String;")
-	  				a = c.call_static(sm)
-					"""
+	  // var code = """c = JAVA.find_class("org/scaloid/hello/HelloScaloid")
+	  // 				sm = c.get_static_method("testMethod","()Ljava/lang/String;")
+	  // 				a = c.call_static(sm)
+	  // 					"""
 		  
-	  var jamRuby: Jamruby = new Jamruby()
-	  var ps: ParserState = jamRuby.parse(code)
-	  var ret: Value = jamRuby.run(ps)
+	  // var jamRuby: Jamruby = new Jamruby()
+	  // var ps: ParserState = jamRuby.parse(code)
+	  // var ret: Value = jamRuby.run(ps)
+	  
+	  TestScalaObject.testMethod
      
-	  STextView(ret.toString(jamRuby.state()))
+	  // STextView(ret.toString(jamRuby.state()))
       STextView("Me too")
       STextView("I am taller than you") textSize 15.dip // overriding
       SEditText("Yellow input field")
       SButton(R.string.red)
     } padding 20.dip
   }
-  
-  def coolToast():String= {
-  		toast("carai")
-  		"lol"
-  	}
+
   
   // def findRubyFile(): Seq[File] = {
   // 	  val finder: PathFinder = ("aa" / "ruby") ** "*.rb"
