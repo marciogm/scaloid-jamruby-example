e751a903ce22f590f5b3b6d59684e47b30e5d06e
marciogm <marciogiaxa@gmail.com>
2013-10-17 21:29:59 -0300

    trying other way to show toast

--- src/org/scaloid/hello/HelloScaloid.scala
+++ src/org/scaloid/hello/HelloScaloid.scala
@@ -15,75 +15,53 @@ import org.jamruby.mruby.MRuby
 import org.jamruby.mruby.ParserState
 import org.jamruby.mruby.Value
  
+object CallUIMethodsFromClass extends HelloScaloid {
+	// val TEST_SYMBOL = "*"
+	// override def basis = this
+	// override implicit val ctx = this
+
+	// def SetContext(c : Context) = {
+	// 	ctx = c
+	// }
+	// 
+	// def GetContext() : Context = {
+	// 	ctx
+	// }
+
+	def callToast() : Unit = {
+		showToast()
+	}
+}
 
  
 class HelloScaloid extends SActivity {
-	
-	object TestScalaObject {
-		// val TEST_SYMBOL = "*"
-		// override def basis = this
-		// override implicit val ctx = this
-	
-		// def SetContext(c : Context) = {
-		// 	ctx = c
-		// }
-		// 
-		// def GetContext() : Context = {
-		// 	ctx
-		// }
-	
-		def testMethod() = {
-			toast("marcio")
-			"lol"
-		}
-	}
- 
-  onCreate {	
-	// TestScalaObject.SetContext(this)
-	
+	 
+  onCreate {
     contentView = new SVerticalLayout {
       style {
-        // case b: SButton => b.textColor(Color.RED).onClick(loading())
         case t: STextView => t textSize 10.dip
         case v => v.backgroundColor(Color.YELLOW)
       }  
 	  
+	  var code = """c = JAVA.find_class("org/scaloid/hello/CallUIMethodsFromClass")
+	  				sm = c.get_static_method("callToast","()V")
+	  				"""
+	  		  
+	  var jamRuby: Jamruby = new Jamruby()
+	  var ps: ParserState = jamRuby.parse(code)
+	  var ret: Value = jamRuby.run(ps)
+	  //      
 	  
-	  // javap -s -bootclasspath classes.jar -classpath bin/classes org.scaloid.common.SToast
-	  
-  	  // W/dalvikvm(10144): JNI WARNING: can't call Landroid/widget/Toast;.makeText on instance of Lorg/scaloid/hello/HelloScaloid$$anonfun$2$$anon$1;
-  	  // W/dalvikvm(10144):              in Lorg/jamruby/mruby/MRuby;.n_run:(JJLorg/jamruby/mruby/Value;)Lorg/jamruby/mruby/Value; (CallObjectMethodA)
-  
-  	  // var code = """c = JAVA.find_class("org/scaloid/hello/TestScalaObject")
-  	  // 				sm = c.get_static_method("GetContext","()Lorg/scaloid/common/SVerticalLayout;")
-  	  // 					o = c.call_static(sm)
-  	  // 					to = JAVA.find_class("android/widget/Toast")
-  	  // 					im = to.get_static_method("makeText","(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;")
-  	  // 					r = to.call(o,im,"Hello, Ruby!",100)
-  	  // 
-	  
-	  // var code = """c = JAVA.find_class("org/scaloid/hello/HelloScaloid")
-	  // 				sm = c.get_static_method("testMethod","()Ljava/lang/String;")
-	  // 				a = c.call_static(sm)
-	  // 					"""
-	  // 		  
-	  // var jamRuby: Jamruby = new Jamruby()
-	  // var ps: ParserState = jamRuby.parse(code)
-	  // var ret: Value = jamRuby.run(ps)
-	  
-	  TestScalaObject.testMethod
-     
-	  // STextView(ret.toString(jamRuby.state()))
+	  STextView(ret.toString(jamRuby.state()))
       STextView("Me too")
       STextView("I am taller than you") textSize 15.dip // overriding
       SEditText("Yellow input field")
       SButton(R.string.red)
     } padding 20.dip
   }
-
   
-  // def findRubyFile(): Seq[File] = {
-  // 	  val finder: PathFinder = ("aa" / "ruby") ** "*.rb"
-  // 	  finder.get
-  // }
+  def showToast() : Unit = {
+	  toast("teste")
+  }
+
 }
