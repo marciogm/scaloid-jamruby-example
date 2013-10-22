5b7e24661defab742eb1f1d7b2c179abd2246bde
marciogm <marciogiaxa@gmail.com>
2013-10-22 18:35:49 -0200

    find class from object

--- src/org/scaloid/hello/HelloScaloid.scala
+++ src/org/scaloid/hello/HelloScaloid.scala
@@ -17,39 +17,47 @@ import org.jamruby.mruby.Value
 
 
 object CallUIMethodsFromClass {
-	
-	def callToast(implicit activity: Activity) = {
-		toast("It's works! :D")
+		
+	def callToast(implicit activity : Activity) = {
+		toast("Ok")
 	}
 }
-
  
 class HelloScaloid extends SActivity {
+ 
+  object HelloScaloid {
+	  def getCurrentCtx() = {
+		  ctx
+	  }
+  }
 	 
-  onCreate {
+  onCreate {	
     contentView = new SVerticalLayout {
       style {
         case t: STextView => t textSize 10.dip
         case v => v.backgroundColor(Color.YELLOW)
       }  
 	  
-	  // var code = """c = JAVA.find_class("org/scaloid/hello/CallUIMethodsFromClass")
-	  // 				sm = c.get_static_method("callToast","()V")
-	  // 					a = c.call_static(sm)
-	  // 				"""
-	  // 		  
-	  // var jamRuby: Jamruby = new Jamruby()
-	  // var ps: ParserState = jamRuby.parse(code)
-	  // var ret: Value = jamRuby.run(ps)
+	  var code = """c = JAVA.find_class("org.scaloid.hello.HelloScaloid\$HelloScaloid\$")
+	  				obj = c.get_static_method("getCurrentCtx","()Lorg/scaloid/common/SActivity;")
+	  			 """
+	  		  
+	  var jamRuby: Jamruby = new Jamruby()
+	  var ps: ParserState = jamRuby.parse(code)
+	  var ret: Value = jamRuby.run(ps)
 	  
 	  
-	  CallUIMethodsFromClass.callToast	  
+	  // CallUIMethodsFromClass.getCtxFromApp
 	  
-	  // STextView(ret.toString(jamRuby.state()))
+	  STextView(ret.toString(jamRuby.state()))
       STextView("Me too")
       STextView("I am taller than you") textSize 15.dip // overriding
       SEditText("Yellow input field")
       SButton(R.string.red)
     } padding 20.dip
+  }
+  
+  def getCu2rrentCtx() : SActivity = {
+	  ctx
   }
 }
